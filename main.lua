65e15005f09ec9c2aa02505695ac266b524f87d2
whymed <saucedadmiguel@gmail.com>
2013-11-26 20:07:31 -0800

    0.1
    Todo comienza aqui

---
+++ main.lua
@@ -0,0 +1,26 @@
+function love.load()
+end
+
+function love.draw()
+end
+
+function love.update(dt)
+end
+
+function love.focus(bool)
+end
+
+function love.keypressed( key, unicode )
+end
+
+function love.keyreleased( key, unicode )
+end
+
+function love.mousepressed( x, y, button )
+end
+
+function love.mousereleased( x, y, button )
+end
+
+function love.quit()
+end
